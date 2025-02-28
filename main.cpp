0bc45a12dc6f28364362fa0c007b320530e8d7d4
Aisleman <davidailemen7@gmail.com>
2025-02-28 01:27:10 +0000

    Stage 4 committed

--- main.cpp
+++ main.cpp
@@ -3,12 +3,14 @@
 #include <sstream>
 #include <vector>
 #include <iomanip>
+#include <map>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 struct Employee
 {
-    int employeeId;
+    int id;
     string name;
     string department;
     int age;
@@ -39,7 +41,7 @@ void getEmployeesFromCSV(const string& filename, vector<Employee> &employees)
         Employee employee;
         string value;
 
-        getline(ss, value, ','); employee.employeeId = stoi(value);
+        getline(ss, value, ','); employee.id = stoi(value);
         getline(ss, employee.name, ',');
         getline(ss, employee.department, ',');
         getline(ss, value, ','); employee.age = stoi(value);
@@ -61,7 +63,7 @@ void display(const vector<Employee> &employees)
 
     for (const auto& employee : employees)
     {
-        cout << left << setw(10) << employee.employeeId
+        cout << left << setw(10) << employee.id
                      << setw(20) << employee.name
                      << setw(20) << employee.department
                      << setw(10) << employee.age
@@ -70,25 +72,214 @@ void display(const vector<Employee> &employees)
     }
 }
 
-void searchByAge(const vector<Employee> &employees, int age)
+int searchByID(const vector<Employee> &employees, int id)
 {
-    for (const Employee &employee : employees)
+    for (int i = 0; i < employees.size(); i++)
     {
-        if (employee.age == age)
+        if (employees[i].id == id)
         {
-            cout << "Employee ID: " << employee.employeeId << endl;
-            cout << "Name: " << employee.name << endl;
-            cout << "Department: " << employee.department << endl;
-            cout << "Age: " << employee.age << endl;
-            cout << "Salary: " << employee.salary << endl;
-            cout << endl;
+            return i;
+        }
+    }
+    return -1;
+}
+
+void displayEmployeeByID(const vector<Employee> &employees, int id)
+{
+    int index = searchByID(employees, id);
+    if (index == -1)
+    {
+        cout << "Employee not found" << endl;
+    }
+    else
+    {
+        cout << "Employee Information" << endl;
+        cout << left << setw(10) << "ID"
+                     << setw(20) << "Name"
+                     << setw(20) << "Department"
+                     << setw(10) << "Age"
+                     << setw(10) << "Salary" << endl;
+        cout << string(70, '-') << endl;
+
+        cout << left << setw(10) << employees[index].id
+                     << setw(20) << employees[index].name
+                     << setw(20) << employees[index].department
+                     << setw(10) << employees[index].age
+                     << setw(10) << employees[index].salary << endl;
+        cout << string(70, '-') << endl;
+    }
+}
+
+map<string, int> countByDepartment(const vector<Employee> &employees)
+{
+    map<string, int> departmentCount;
+    for (const auto& employee : employees)
+    {
+        departmentCount[employee.department]++;
+    }
+    return departmentCount;
+}
+
+void displayDepartmentCount(const map<string, int> &departmentCount)
+{
+    cout << "Department Count" << endl;
+    cout << left << setw(20) << "Department"
+                 << setw(10) << "Count" << endl;
+    cout << string(30, '-') << endl;
+
+    for (const auto& [department, count] : departmentCount)
+    {
+        cout << left << setw(20) << department
+                     << setw(10) << count << endl;
+        cout << string(30, '-') << endl;
+    }
+}
+
+void displayByDepartment(const vector<Employee> &employees, const string& department)
+{
+    cout << "Employee List by Department" << endl;
+    cout << left << setw(10) << "ID"
+                 << setw(20) << "Name"
+                 << setw(20) << "Department"
+                 << setw(10) << "Age"
+                 << setw(10) << "Salary" << endl;
+    cout << string(70, '-') << endl;
+
+    for (const auto& employee : employees)
+    {
+        if (employee.department == department)
+        {
+            cout << left << setw(10) << employee.id
+                         << setw(20) << employee.name
+                         << setw(20) << employee.department
+                         << setw(10) << employee.age
+                         << setw(10) << employee.salary << endl;
+            cout << string(70, '-') << endl;
+        }
+    }
+}
+
+void displayStatsByAge(const vector<Employee> &employees, Employee& youngest, Employee& oldest)
+{
+    if (employees.empty())
+    {
+        cout << "No employees found" << endl;
+    }
+
+    youngest = employees[0];
+    oldest = employees[0];
+    int totalAge = 0;
+
+    for (const auto& employee : employees)
+    {
+        totalAge += employee.age;
+        if (employee.age < youngest.age)
+        {
+            youngest = employee;
+        }
+        if (employee.age > oldest.age)
+        {
+            oldest = employee;
         }
     }
 
+    cout << "Oldest Employee" << endl;
+    cout << "ID: " << oldest.id
+         <<", Name: " << oldest.name
+         << ", Department: " << oldest.department
+         << ", Age: " << oldest.age
+         << ", Salary: " << oldest.salary << endl;
+
+    cout << "Youngest Employee" << endl;
+    cout << "ID: " << youngest.id
+         <<", Name: " << youngest.name
+         << ", Department: " << youngest.department
+         << ", Age: " << youngest.age
+         << ", Salary: " << youngest.salary << endl;
+
+    cout << "Average Age: " << totalAge / employees.size() << endl;
+}
+
+vector<Employee> searchEmployeesByName(const vector<Employee> &employees, const string& name)
+{
+    vector<Employee> result;
+    for (const auto& employee : employees)
+    {
+        if (employee.name == name)
+        {
+            result.push_back(employee);
+        }
+    }
+    return result;
+}
+
+void displaySearchResults(const vector<Employee> &employees, const vector<Employee> &result)
+{
+    if (result.empty())
+    {
+        cout << "No employees found" << endl;
+        return;
+    }
+    else {
+        cout << "Search Results" << endl;
+        cout << left << setw(10) << "ID"
+                     << setw(20) << "Name"
+                     << setw(20) << "Department"
+                     << setw(10) << "Age"
+                     << setw(10) << "Salary" << endl;
+        cout << string(70, '-') << endl;
+
+        for (const auto& employee : result)
+        {
+            cout << left << setw(10) << employee.id
+                         << setw(20) << employee.name
+                         << setw(20) << employee.department
+                         << setw(10) << employee.age
+                         << setw(10) << employee.salary << endl;
+            cout << string(70, '-') << endl;
+        }
+    }
+}
+
+void sortEmployeesBySalary(vector<Employee> &employees)
+{
+    sort(employees.begin(), employees.end(), [](const Employee& a, const Employee& b) {
+        return a.salary < b.salary;
+    });
+}
+
+void displaySortedEmployees(const vector<Employee> &employees)
+{
+    cout << "Employee List Sorted by Salary" << endl;
+    cout << left << setw(10) << "ID"
+                 << setw(20) << "Name"
+                 << setw(20) << "Department"
+                 << setw(10) << "Age"
+                 << setw(10) << "Salary" << endl;
+    cout << string(70, '-') << endl;
+
+    for (const auto& employee : employees)
+    {
+        cout << left << setw(10) << employee.id
+                     << setw(20) << employee.name
+                     << setw(20) << employee.department
+                     << setw(10) << employee.age
+                     << setw(10) << employee.salary << endl;
+        cout << string(70, '-') << endl;
+    }
 }
 int main()
 {
     vector<Employee> employees;
     getEmployeesFromCSV("employee.csv", employees);
     display(employees);
+    displayEmployeeByID(employees, searchByID(employees, 17));
+    displayDepartmentCount(countByDepartment(employees));
+    displayByDepartment(employees, "HR");
+    Employee youngest, oldest;
+    displayStatsByAge(employees, youngest, oldest);
+    displaySearchResults(employees, searchEmployeesByName(employees, "John"));
+    sortEmployeesBySalary(employees);
+    displaySortedEmployees(employees);
+    return 0;
 }
